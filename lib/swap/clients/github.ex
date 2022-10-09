0e754f3fbd2f915036733933317b451c41380f85
Roberto Ribeiro <robertoribeiro.code@gmail.com>
2022-10-09 12:33:02 -0300

    feat: adicionando função para verificar se o limite foi atingido no provedores

--- lib/swap/clients/github.ex
+++ lib/swap/clients/github.ex
@@ -3,11 +3,20 @@ defmodule Swap.Clients.Github do
 
   @type response :: {:ok | :error, list() | struct()}
 
-  @callback repo_issues(owner :: String.t(), repo :: String.t()) :: response
-  @callback repo_contributors(owner :: String.t(), repo :: String.t()) :: response
+  @callback repo_issues(owner :: String.t(), repo :: String.t(), token :: String.t() | nil) ::
+              response()
 
-  def repo_issues(owner, repo), do: adapter().repo_issues(owner, repo)
-  def repo_contributors(owner, repo), do: adapter().repo_contributors(owner, repo)
+  @callback repo_contributors(owner :: String.t(), repo :: String.t(), token :: String.t() | nil) ::
+              response()
+
+  @callback rate_limit(token :: String.t() | nil) :: response()
+
+  def repo_issues(owner, repo, token \\ nil), do: adapter().repo_issues(owner, repo, token)
+
+  def repo_contributors(owner, repo, token \\ nil),
+    do: adapter().repo_contributors(owner, repo, token)
+
+  def rate_limit(token \\ nil), do: adapter().rate_limit(token)
 
   def adapter, do: Application.fetch_env!(:swap, :client_github_adapter)
 end
--- lib/swap/clients/github/mock.ex
+++ lib/swap/clients/github/mock.ex
@@ -6,7 +6,9 @@ defmodule Swap.Clients.Github.Mock do
   alias Swap.Clients.Github.Response
 
   @impl true
-  def repo_issues(_owner, "valid_repo", _token) do
+  def repo_issues(owner, repo, _token), do: repo_issues(owner, repo)
+
+  def repo_issues(_owner, "valid_repo") do
     {:ok,
      [
        %Response.Issue{
@@ -18,16 +20,18 @@ defmodule Swap.Clients.Github.Mock do
      ]}
   end
 
-  def repo_issues(_owner, "invalid_repo", _token) do
+  def repo_issues(_owner, "invalid_repo") do
     {:error,
-     %Swap.Clients.Github.Response.Error{
+     %Response.Error{
        reason: "Error",
        status: 500
      }}
   end
 
   @impl true
-  def repo_contributors(_owner, "valid_repo", _token) do
+  def repo_contributors(owner, repo, _token), do: repo_contributors(owner, repo)
+
+  def repo_contributors(_owner, "valid_repo") do
     {:ok,
      [
        %Response.Contributor{
@@ -39,11 +43,32 @@ defmodule Swap.Clients.Github.Mock do
      ]}
   end
 
-  def repo_contributors(_owner, "invalid_repo", _token) do
+  def repo_contributors(_owner, "invalid_repo") do
     {:error,
-     %Swap.Clients.Github.Response.Error{
+     %Response.Error{
        reason: "Error",
        status: 500
+     }}
+  end
+
+  @impl true
+  def rate_limit(nil) do
+    {:ok,
+     %Response.RateLimit{
+       limit: 5000,
+       remaining: 0,
+       reset: 1_665_327_096,
+       used: 5000
+     }}
+  end
+
+  def rate_limit("token") do
+    {:ok,
+     %Response.RateLimit{
+       limit: 5000,
+       remaining: 4997,
+       reset: 1_665_327_096,
+       used: 3
      }}
   end
 end
